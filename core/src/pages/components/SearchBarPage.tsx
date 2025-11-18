cb5c2c80012aa8c513180aa7bde29b9e925e1cc6
Muhammed Semih Irmak <smhirmak@hotmail.com>
2025-11-18 19:59:46 +0300

    change search bar import

--- core/src/pages/components/SearchBarPage.tsx
+++ core/src/pages/components/SearchBarPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import SearchBar from '@/components/SearchBar';
+import SearchBar from '@/components/SearchBar-1';
 
 const SearchBarPage = () => {
   const [value, setValue] = React.useState('');
