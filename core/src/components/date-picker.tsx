e6735e7dfd2542d7b03ad4aa266e532f290c1f92
MIRMAKNTB3\Semih <mirmak@trabilisim.com>
2025-11-19 17:42:56 +0300

    change name

--- core/src/components/date-picker.tsx
+++ core/src/components/date-picker.tsx
@@ -7,7 +7,7 @@ import { DateRange } from 'react-day-picker';
 import { CalendarBlankIcon } from '@phosphor-icons/react';
 import { cn } from '@/lib/utils';
 import Button from '@/components/button';
-import Calendar from '@/components/Calendar';
+import Calendar from '@/components/Calendar-1';
 import { Popover, PopoverContent, PopoverTrigger } from './popover';
 import Label from './label';
 
